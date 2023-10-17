cb1acadd7a3f0e01fce05c3fe91cc035e4398369
Llonvne <86454692+Llonvne@users.noreply.github.com>
2023-10-18 01:56:45 +0800

    double fault

---
+++ src/display.rs
@@ -0,0 +1,9 @@
+use crate::println;
+
+pub fn line() {
+    println!("===== ===== ===== ===== ===== =====");
+}
+
+fn error(content:&str){
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use blog_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+
+    blog_os::init();
+
+    blog_os::display::line();
+    println!("RustOS 0.0.1,Welcome!");
+    blog_os::display::line();
+    println!("\n");
+
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 42;
+    // };
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
